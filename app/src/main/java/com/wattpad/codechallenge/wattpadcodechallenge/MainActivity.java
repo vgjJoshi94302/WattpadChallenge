8c2a5ba0515be7af70e75efed18f6902181a7e6c
vkoth <vkothiwala6@gmail.com>
2019-03-06 02:49:54 -0500

    with searchbox and internet checker

--- app/src/main/java/com/wattpad/codechallenge/wattpadcodechallenge/MainActivity.java
+++ app/src/main/java/com/wattpad/codechallenge/wattpadcodechallenge/MainActivity.java
@@ -1,5 +1,10 @@
 package com.wattpad.codechallenge.wattpadcodechallenge;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -7,11 +12,15 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,7 +28,10 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView tvNointernet;
+    SearchView searchView;
+    ProgressBar progressBar;
+
     ArrayList<Story> storiesModelList = new ArrayList<>();
     StoriesAdapter storiesAdapter;
 
@@ -28,11 +40,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        makeAPIcall();
+        tvNointernet = (TextView) findViewById(R.id.tv_no_internet);
+        searchView = (SearchView) findViewById(R.id.search);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
 
-        //textView = (TextView) findViewById(R.id.tv_stories);
+
+        if (checkConnection()) {
+
+            tvNointernet.setVisibility(View.INVISIBLE);
+            makeAPIcall();
+
+        } else {
+            progressBar.setVisibility(View.INVISIBLE);
+
+        }
 
 
+    }
+
+    private boolean checkConnection() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
     private void makeAPIcall() {
@@ -49,13 +79,12 @@ public class MainActivity extends AppCompatActivity {
                 for (int i = 0; i < storiesModelList.size(); i++) {
 
 
-                    Log.d("Main",i + 1 + ">" + storiesModelList.get(i).getTitle() + "\n");
+                    Log.d("Main", i + 1 + ">" + storiesModelList.get(i).getTitle() + "\n");
 
                 }
 
 
-
-                storiesAdapter = new StoriesAdapter(storiesModelList,getApplicationContext());
+                storiesAdapter = new StoriesAdapter(storiesModelList, getApplicationContext());
                 RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
                 LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
                 recyclerView.setLayoutManager(linearLayoutManager);
@@ -65,14 +94,28 @@ public class MainActivity extends AppCompatActivity {
 
                 recyclerView.setAdapter(storiesAdapter);
 
-                    Log.d("Main","length="+storiesModelList.size());
+                searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                    @Override
+                    public boolean onQueryTextSubmit(String query) {
+                        return false;
+                    }
 
+                    @Override
+                    public boolean onQueryTextChange(String newText) {
+                        storiesAdapter.getFilter().filter(newText);
+                        return false;
+                    }
+                });
+
+                Log.d("Main", "length=" + storiesModelList.size());
+                progressBar.setVisibility(View.INVISIBLE);
 
             }
 
             @Override
             public void onFailure(Call<StoriesModel> call, Throwable t) {
                 Log.d("Main", "fail");
+
             }
         });
     }
--- app/src/main/java/com/wattpad/codechallenge/wattpadcodechallenge/StoriesAdapter.java
+++ app/src/main/java/com/wattpad/codechallenge/wattpadcodechallenge/StoriesAdapter.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,16 +18,18 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 import java.util.ArrayList;
 
-public class StoriesAdapter extends RecyclerView.Adapter<StoriesAdapter.MyViewHolder> {
+public class StoriesAdapter extends RecyclerView.Adapter<StoriesAdapter.MyViewHolder> implements Filterable {
 
     private ArrayList<Story> storiesList;
+    private ArrayList<Story> storiesListFull;
     Context context;
 
-    public StoriesAdapter(ArrayList<Story> storiesList, Context context)
-    {
+    public StoriesAdapter(ArrayList<Story> storiesList, Context context) {
         this.storiesList = storiesList;
         this.context = context;
+        storiesListFull = new ArrayList<>(storiesList);
     }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -39,10 +43,10 @@ public class StoriesAdapter extends RecyclerView.Adapter<StoriesAdapter.MyViewHo
     public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
 
 
-    Story story = storiesList.get(position);
+        Story story = storiesList.get(position);
 
-    holder.title.setText(story.getTitle());
-    holder.author.setText(story.getUser().getFullname());
+        holder.title.setText(story.getTitle());
+        holder.author.setText(story.getUser().getFullname());
 
         Glide.with(context)
                 .load(story.getCover())
@@ -56,6 +60,44 @@ public class StoriesAdapter extends RecyclerView.Adapter<StoriesAdapter.MyViewHo
         // Log.d("Story","Count"+storiesList.size());
         return storiesList.size();
     }
+
+    @Override
+    public Filter getFilter() {
+        return storiesFilter;
+    }
+
+    private Filter storiesFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<Story> filteredList = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                filteredList.addAll(storiesListFull);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for(Story item : storiesListFull)
+                {
+                    if(item.getTitle().toLowerCase().contains(filterPattern))
+                    {
+                        filteredList.add(item);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+            storiesList.clear();
+            storiesList.addAll((ArrayList)results.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView title, author;
